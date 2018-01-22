55185b1e8ff89f8e84d75279d6f21da10daacd64
Filipe Forattini <filipeforattini1@gmail.com>
2018-01-21 23:00:08 -0200

    feat(jds): requirements for front-end

--- src/components/views/jds/FrontEnd.vue
+++ src/components/views/jds/FrontEnd.vue
@@ -12,13 +12,13 @@
     <v-content>
 
       <section>
-        <v-parallax src="/static/img/stock/dev-05-1600-black-min.png" height="600">
+        <v-parallax src="/static/img/stock/dev-05-1600-min.png" height="614">
           <v-layout column align-center justify-center class="white--text">
-              <h1 class="white--text mb-2 display-1 text-xs-center paralax-title">
-                Senior Front-End
+              <h1 class="white--text mb-2 display-3 text-xs-center paralax-title">
+                Front-End Pl/Sr
               </h1>
               <v-btn class="blue lighten-2 mt-5" dark large href="mailto:talentos@farmaciaja.com.br">
-                Entrar em contato
+                Entre em contato
               </v-btn>
           </v-layout>
         </v-parallax>
@@ -29,10 +29,7 @@
 
           <v-flex xs12 sm6 class="my-3">
             <div class="text-xs-center">
-              <h2 class="headline">A sua tela</h2>
-              <span class="subheading">
-                vai mudar a forma como os brasileiros compram medicamentos
-              </span>
+              <h2 class="headline">A sua tela vai mudar a forma como os brasileiros compram medicamentos</h2>
             </div>
           </v-flex>
 
@@ -40,55 +37,52 @@
 
             <v-container grid-list-xl>
               <v-layout row wrap align-center>
-                <v-flex xs12 md4>
 
+                <v-flex xs12 md4>
                   <v-card class="elevation-0 transparent">
                     <v-card-text class="text-xs-center">
                       <v-icon x-large class="blue--text text--lighten-2">flash_on</v-icon>
                     </v-card-text>
                     <v-card-title primary-title class="layout justify-center">
-                      <div class="headline text-xs-center">Desenvolva na onda</div>
+                      <div class="headline text-xs-center">Seja pioneiro</div>
                     </v-card-title>
                     <v-card-text>
                       Utilizamos o quê é último do último de tecnologia. Você vai se empolgar com nossas soluções, e, o
                       quê é melhor: você vai aprender muito conosco!
                     </v-card-text>
                   </v-card>
-
                 </v-flex>
-                <v-flex xs12 md4>
 
+                <v-flex xs12 md4>
                   <v-card class="elevation-0 transparent">
                     <v-card-text class="text-xs-center">
-                      <v-icon x-large class="blue--text text--lighten-2">flash_on</v-icon>
+                      <v-icon x-large class="blue--text text--lighten-2">widgets</v-icon>
                     </v-card-text>
                     <v-card-title primary-title class="layout justify-center">
-                      <div class="headline">Fast development</div>
+                      <div class="headline">Encare o desafio</div>
                     </v-card-title>
                     <v-card-text>
-                      Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt ornare.
-                      Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
-                      Nullam in aliquet odio. Aliquam eu est vitae tellus bibendum tincidunt. Suspendisse potenti.
-                    </v-card-text>
-                  </v-card>
-
-                </v-flex>
-                <v-flex xs12 md4>
-
-                  <v-card class="elevation-0 transparent">
-                    <v-card-text class="text-xs-center">
-                      <v-icon x-large class="blue--text text--lighten-2">build</v-icon>
-                    </v-card-text>
-                    <v-card-title primary-title class="layout justify-center">
-                      <div class="headline text-xs-center">Completely Open Sourced</div>
-                    </v-card-title>
-                    <v-card-text>
-                      Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt ornare.
-                      Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
-                      Nullam in aliquet odio. Aliquam eu est vitae tellus bibendum tincidunt. Suspendisse potenti.
+                      Nossa solução atingirá toda a população brasileira. Já trabalhou em um projeto com desafios de
+                      escalabilidade e performance? Aqui é o lugar!
                     </v-card-text>
                   </v-card>
                 </v-flex>
+
+                <v-flex xs12 md4>
+                  <v-card class="elevation-0 transparent">
+                    <v-card-text class="text-xs-center">
+                      <v-icon x-large class="blue--text text--lighten-2">healing</v-icon>
+                    </v-card-text>
+                    <v-card-title primary-title class="layout justify-center">
+                      <div class="headline text-xs-center">Mude vidas</div>
+                    </v-card-title>
+                    <v-card-text>
+                      Todos nós somos consumidores de remédios. Mas certamente não compramos com os melhores preços, ou
+                      planos de recorrências!
+                    </v-card-text>
+                  </v-card>
+                </v-flex>
+
               </v-layout>
             </v-container>
           </v-flex>
@@ -96,18 +90,153 @@
       </section>
 
       <section>
-        <v-parallax src="assets/section.jpg" height="380">
+        <v-parallax src="/static/img/stock/dev-06-1600-min.png" height="340">
           <v-layout column align-center justify-center>
-            <div class="headline white--text mb-3 text-xs-center">Web development has never been easier</div>
-            <em>Kick-start your application today</em>
-            <v-btn
-              class="blue lighten-2 mt-5"
-              dark
-              large
-              href="/pre-made-themes"
-            >
-              Get Started
-            </v-btn>
+            <div class="headline white--text mb-3 text-xs-center">Não trabalhe, entre conosco em uma missão!</div>
+              <v-btn class="blue lighten-2 mt-5" dark large href="mailto:talentos@farmaciaja.com.br">
+                Entrar em contato
+              </v-btn>
+          </v-layout>
+        </v-parallax>
+      </section>
+
+      <section>
+        <v-layout column wrap class="my-5" align-center>
+
+          <v-flex xs12 sm4 class="my-3">
+            <div class="text-xs-center">
+              <h2 class="headline">Suas responsabilidades</h2>
+            </div>
+          </v-flex>
+
+          <v-flex xs12>
+            <v-container grid-list-xl>
+              <v-layout row wrap align-center>
+
+                <v-flex xs12 md4>
+                  <v-card class="elevation-0 transparent">
+                    <v-card-text class="text-xs-center">
+                      <v-icon x-large class="blue--text text--lighten-2">flash_on</v-icon>
+                    </v-card-text>
+                    <v-card-title primary-title class="layout justify-center">
+                      <div class="headline text-xs-center">ES6 JS</div>
+                    </v-card-title>
+                    <v-card-text>
+                      Desenvolvimento de SPAs em Vue.js ou React com o último que existe de boas práticas e tecnologia
+                      como Webpack e postcss!
+                    </v-card-text>
+                  </v-card>
+                </v-flex>
+
+                <v-flex xs12 md4>
+                  <v-card class="elevation-0 transparent">
+                    <v-card-text class="text-xs-center">
+                      <v-icon x-large class="blue--text text--lighten-2">widgets</v-icon>
+                    </v-card-text>
+                    <v-card-title primary-title class="layout justify-center">
+                      <div class="headline">Componentiação & Storybook</div>
+                    </v-card-title>
+                    <v-card-text>
+                      Adoramos arquitetura e reutilização de código para aumento de produtividade. Você será peça chave
+                      na montagem dessa estrutura!
+                    </v-card-text>
+                  </v-card>
+                </v-flex>
+
+                <v-flex xs12 md4>
+                  <v-card class="elevation-0 transparent">
+                    <v-card-text class="text-xs-center">
+                      <v-icon x-large class="blue--text text--lighten-2">healing</v-icon>
+                    </v-card-text>
+                    <v-card-title primary-title class="layout justify-center">
+                      <div class="headline text-xs-center">Mobile first</div>
+                    </v-card-title>
+                    <v-card-text>
+                      Todo aplicativo e resposta dele devem ser focados na experiência mobile de nossos usuários.
+                      Principalmente quando se pensa em SEO!
+                    </v-card-text>
+                  </v-card>
+                </v-flex>
+
+              </v-layout>
+            </v-container>
+          </v-flex>
+
+        </v-layout>
+      </section>
+
+      <section>
+        <v-layout column wrap class="my-5" align-center>
+
+          <v-flex xs12 sm6 class="my-3">
+            <div class="text-xs-center">
+              <h2 class="headline">Pré requisitos</h2>
+            </div>
+          </v-flex>
+
+          <v-flex xs12>
+            <v-container grid-list-xl>
+              <v-layout row wrap align-center>
+
+                <v-flex xs12 md4>
+                  <v-card class="elevation-0 transparent">
+                    <v-card-text class="text-xs-center">
+                      <v-icon x-large class="blue--text text--lighten-2">flash_on</v-icon>
+                    </v-card-text>
+                    <v-card-title primary-title class="layout justify-center">
+                      <div class="headline text-xs-center">Experiência</div>
+                    </v-card-title>
+                    <v-card-text>
+                      Ao menos 2 anos com experiência em desenvolvimento de webservices from um Front-End similar ao
+                      proposto acima.
+                    </v-card-text>
+                  </v-card>
+                </v-flex>
+
+                <v-flex xs12 md4>
+                  <v-card class="elevation-0 transparent">
+                    <v-card-text class="text-xs-center">
+                      <v-icon x-large class="blue--text text--lighten-2">widgets</v-icon>
+                    </v-card-text>
+                    <v-card-title primary-title class="layout justify-center">
+                      <div class="headline">Time Ágil</div>
+                    </v-card-title>
+                    <v-card-text>
+                      Metodologias ágeis - como Scrum e Squads, principalmente. Mas o principal é ser um solucionador
+                      de problemas.
+                    </v-card-text>
+                  </v-card>
+                </v-flex>
+
+                <v-flex xs12 md4>
+                  <v-card class="elevation-0 transparent">
+                    <v-card-text class="text-xs-center">
+                      <v-icon x-large class="blue--text text--lighten-2">healing</v-icon>
+                    </v-card-text>
+                    <v-card-title primary-title class="layout justify-center">
+                      <div class="headline text-xs-center">Inglês Intermediário</div>
+                    </v-card-title>
+                    <v-card-text>
+                      Afinal toda documentação está mínimamente em inglês. Não é um requisito eliminatório, mas
+                      certamente aumenta a produtividade.
+                    </v-card-text>
+                  </v-card>
+                </v-flex>
+
+              </v-layout>
+            </v-container>
+          </v-flex>
+
+        </v-layout>
+      </section>
+
+      <section>
+        <v-parallax src="/static/img/stock/dev-04-1600-min.png" height="340">
+          <v-layout column align-center justify-center>
+            <div class="headline white--text mb-3 text-xs-center">Venha trabalhar conosco!</div>
+              <v-btn class="blue lighten-2 mt-5" dark large href="mailto:talentos@farmaciaja.com.br">
+                Entre em contato
+              </v-btn>
           </v-layout>
         </v-parallax>
       </section>
@@ -118,38 +247,32 @@
             <v-flex xs12 sm4>
               <v-card class="elevation-0 transparent">
                 <v-card-title primary-title class="layout justify-center">
-                  <div class="headline">Company info</div>
+                  <iframe
+                    width="400"
+                    height="258"
+                    frameborder="0"
+                    style="border:0"
+                    allowfullscreen
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d914.3494861009034!2d-46.662537577503116!3d-23.554147130267197!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce582dc6b93b57%3A0x410700cc6a1d872f!2sAv.+Ang%C3%A9lica%2C+2529+-+Bela+Vista%2C+S%C3%A3o+Paulo+-+SP%2C+01227-200!5e0!3m2!1sen!2sbr!4v1516574874501"
+                  ></iframe>
                 </v-card-title>
-                <v-card-text>
-                  Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt ornare.
-                  Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
-                  Nullam in aliquet odio. Aliquam eu est vitae tellus bibendum tincidunt. Suspendisse potenti.
-                </v-card-text>
               </v-card>
             </v-flex>
             <v-flex xs12 sm4 offset-sm1>
               <v-card class="elevation-0 transparent">
                 <v-card-title primary-title class="layout justify-center">
-                  <div class="headline">Contact us</div>
+                  <div class="headline">Contato</div>
                 </v-card-title>
                 <v-card-text>
-                  Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt ornare.
+                  Que tal um papo no nosso escritório? Agende conosco usando o e-mail abaixo!
                 </v-card-text>
                 <v-list class="transparent">
-                  <v-list-tile>
-                    <v-list-tile-action>
-                      <v-icon class="blue--text text--lighten-2">phone</v-icon>
-                    </v-list-tile-action>
-                    <v-list-tile-content>
-                      <v-list-tile-title>777-867-5309</v-list-tile-title>
-                    </v-list-tile-content>
-                  </v-list-tile>
                   <v-list-tile>
                     <v-list-tile-action>
                       <v-icon class="blue--text text--lighten-2">place</v-icon>
                     </v-list-tile-action>
                     <v-list-tile-content>
-                      <v-list-tile-title>Chicago, US</v-list-tile-title>
+                      <v-list-tile-title>São Paulo, SP</v-list-tile-title>
                     </v-list-tile-content>
                   </v-list-tile>
                   <v-list-tile>
@@ -157,7 +280,7 @@
                       <v-icon class="blue--text text--lighten-2">email</v-icon>
                     </v-list-tile-action>
                     <v-list-tile-content>
-                      <v-list-tile-title>john@vuetifyjs.com</v-list-tile-title>
+                      <v-list-tile-title>talentos@farmaciaja.com.br</v-list-tile-title>
                     </v-list-tile-content>
                   </v-list-tile>
                 </v-list>
@@ -167,18 +290,6 @@
         </v-container>
       </section>
 
-      <v-footer class="blue darken-2">
-        <v-layout row wrap align-center>
-          <v-flex xs12>
-            <div class="white--text ml-3">
-              Made with
-              <v-icon class="red--text">favorite</v-icon>
-              by <a class="white--text" href="https://vuetifyjs.com" target="_blank">Vuetify</a>
-              and <a class="white--text" href="https://github.com/vwxyzjn">Costa Huang</a>
-            </div>
-          </v-flex>
-        </v-layout>
-      </v-footer>
     </v-content>
   </v-app>
 </template>
@@ -196,7 +307,7 @@ export default {
 
 <style scoped>
   .paralax-title {
-    margin-top: 8em;
+    margin-top: 5em;
   }
 
   .transparent {
